f63750e56ea4c3994f12548b908a8c14a9e1102e
Zeak218 <iggy.torres@yahoo.com>
2023-06-05 23:19:25 +0800

    Added state to Login & SignUp

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,6 +1,16 @@
+import { useState } from 'react'
 import './Login.css'
 
 export default function Login () {
+    
+    const [email, setEmail] = useState('')
+    const [password, setPassword] = useState('')
+
+    const onSubmit = (e) => {
+        e.preventDefault();
+        console.log( {email, password})
+    }
+
     return (
         <div className='login-body'>
             <div className='home-image-container'>
@@ -10,11 +20,11 @@ export default function Login () {
                 <div>
                     <h1 className='form-title'>Login</h1>
                 </div>
-                <form className='form-contents'>
-                    <input type='email' placeholder='Enter your email...'/>
+                <form className='form-contents' onSubmit={onSubmit}>
+                    <input type='email' value={email} placeholder='Enter your email...' onChange={(e) => {setEmail(e.target.value)}} />
                     <br />
                     <br />
-                    <input type='password' placeholder='Enter your password...' />
+                    <input type='password' value={password} placeholder='Enter your password...' onChange={(e) => {setPassword(e.target.value)}} />
                     <br />
                     <br />
                     <button type='submit'>Login</button>
--- src/components/SignUp.jsx
+++ src/components/SignUp.jsx
@@ -1,6 +1,17 @@
+import { useState } from 'react'
 import './SignUp.css'
 
 export default function SignUp () {
+
+    const [email, setEmail] = useState('')
+    const [password, setPassword] = useState('')
+    const [confirmpassword, setConfirmPassword] = useState('')
+
+    const onSubmit = (e) => {
+        e.preventDefault();
+        console.log( {email, password, confirmpassword})
+    }
+
     return (
         <div className='signup-body'>
             <div className='home-image-container'>
@@ -10,14 +21,14 @@ export default function SignUp () {
                 <div>
                     <h1 className='form-title'>Sign Up</h1>
                 </div>
-                <form className='form-contents'>
-                    <input type='email' placeholder='Enter your email...'/>
+                <form className='form-contents' onSubmit={onSubmit}>
+                    <input type='email' value={email} placeholder='Enter your email...' onChange={(e) => {setEmail(e.target.value)}} />
                     <br />
                     <br />
-                    <input type='password' placeholder='Enter your password...' />
+                    <input type='password' value={password} placeholder='Enter your password...' onChange={(e) => {setPassword(e.target.value)}} />
                     <br />
                     <br />
-                    <input type='password' placeholder='Confirm your password...' />
+                    <input type='password' value={confirmpassword} placeholder='Confirm your password...' onChange={(e) => {setConfirmPassword(e.target.value)}} />
                     <br />
                     <br />
                     <button type='submit'>Sign Up</button>
