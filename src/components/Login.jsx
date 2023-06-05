98265bec4107c83b427246b033e3c4646d9f6948
Zeak218 <iggy.torres@yahoo.com>
2023-06-05 23:06:28 +0800

    Added UI

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -3,11 +3,23 @@ import './Login.css'
 export default function Login () {
     return (
         <div className='login-body'>
-            <div className='home-image'>
-                
+            <div className='home-image-container'>
+                <img className='home-image' src = './src/assets/home-bg.png' ></img>
             </div>
             <div className='form-container'>
-
+                <div>
+                    <h1 className='form-title'>Login</h1>
+                </div>
+                <form className='form-contents'>
+                    <input type='email' placeholder='Enter your email...'/>
+                    <br />
+                    <br />
+                    <input type='password' placeholder='Enter your password...' />
+                    <br />
+                    <br />
+                    <button type='submit'>Login</button>
+                </form>
+                <p>Don't have an account? <span>Sign up here!</span></p>
             </div>
         </div>
     )
--- src/components/SignUp.jsx
+++ src/components/SignUp.jsx
@@ -3,7 +3,26 @@ import './SignUp.css'
 export default function SignUp () {
     return (
         <div className='signup-body'>
-            <h1>SignUp</h1>
+            <div className='home-image-container'>
+                <img className='home-image' src = './src/assets/home-bg.png' ></img>
+            </div>
+            <div className='form-container'>
+                <div>
+                    <h1 className='form-title'>Sign Up</h1>
+                </div>
+                <form className='form-contents'>
+                    <input type='email' placeholder='Enter your email...'/>
+                    <br />
+                    <br />
+                    <input type='password' placeholder='Enter your password...' />
+                    <br />
+                    <br />
+                    <input type='password' placeholder='Confirm password...' />
+                    <br />
+                    <br />
+                    <button type='submit'>Sign Up</button>
+                </form>
+            </div>
         </div>
     )
 }
