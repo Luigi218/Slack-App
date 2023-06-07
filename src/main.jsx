e205b1c94ac5d5ecd4b23ee353bcf61ad069a341
Zeak218 <iggy.torres@yahoo.com>
2023-06-07 14:55:31 +0800

    Edited Login & SignUp

--- src/main.jsx
+++ src/main.jsx
@@ -2,11 +2,6 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 
-<<<<<<< HEAD
-=======
-
-
->>>>>>> a2d73a50e7baa21cce1540ee615b66bf9fc0c709
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
